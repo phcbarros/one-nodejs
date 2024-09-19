052477e537a4f2757da63f578eb7d790826aa0b9
Paulo Barros <paulo.barros@lambda3.com.br>
2024-09-19 08:16:54 -0300

    Fundamentos do Node.js - 01 - node --watch

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World!')
+  res.end('Hello Ignite!')
 })
 
 server.listen(3333, () => console.log('Server is running!'))
