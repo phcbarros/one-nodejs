0d54069b6cf1bad84c1fedf76bc9d5841f73b242
Paulo Barros <paulo.barros@lambda3.com.br>
2024-09-23 12:11:54 -0300

    Fundamentos do Node.js - 04 - Separando rotas da aplicação

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,32 +1,19 @@
-import {randomUUID} from 'node:crypto'
 import http from 'node:http'
-import {Database} from './database.js'
-import {json} from './middlewares/json.js'
 
-const database = new Database()
+import {json} from './middlewares/json.js'
+import {routes} from './routes.js'
 
 const server = http.createServer(async (req, res) => {
   const {method, url} = req
 
   await json(req, res)
-  console.log(req.body)
 
-  if (method === 'GET' && url === '/users') {
-    return res.end(JSON.stringify(database.select('users')))
-  }
+  const route = routes.find((route) => {
+    return route.path === url && route.method === method
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const {name, email} = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   res.writeHead(404).end()
