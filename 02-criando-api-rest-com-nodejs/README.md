1314a372cdc6aa91bdd5481cc2026bb27a46845f
Paulo Barros <paulo.barros@lambda3.com.br>
2024-10-09 09:49:19 -0300

    Criando API Rest com Node.js - 02 - Variáveis ambiente

--- 02-criando-api-rest-com-nodejs/README.md
+++ 02-criando-api-rest-com-nodejs/README.md
@@ -2,6 +2,8 @@
 
 ## Migrations
 
+Ferramenta usada [knex](https://knexjs.org/guide/)
+
 ### Criar
 
 ```shell
--- 02-criando-api-rest-com-nodejs/src/server.ts
+++ 02-criando-api-rest-com-nodejs/src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import {knex} from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 5000,
+    })
+    .returning('*')
 
-  return tables
+  return transactions
 })
 
 app.listen({port: 3333}).then(() => {
