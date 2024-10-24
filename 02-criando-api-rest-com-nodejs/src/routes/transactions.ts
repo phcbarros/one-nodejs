c4944c81b72c32874530400398d815afe29bf128
Paulo Barros <paulo.barros@lambda3.com.br>
2024-10-24 14:33:49 -0300

    Criando API Rest com Node.js - 03 - Criação de transações

--- 02-criando-api-rest-com-nodejs/src/routes/transactions.ts
+++ 02-criando-api-rest-com-nodejs/src/routes/transactions.ts
@@ -1,16 +1,26 @@
 import {FastifyInstance} from 'fastify'
-import knex from 'knex'
+import {randomUUID} from 'node:crypto'
+import {z} from 'zod'
+import {knex} from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 5000,
-      })
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const {title, amount, type} = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
--- 02-criando-api-rest-com-nodejs/src/server.ts
+++ 02-criando-api-rest-com-nodejs/src/server.ts
@@ -3,7 +3,9 @@ import {transactionsRoutes} from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running on http://localhost:3333')
