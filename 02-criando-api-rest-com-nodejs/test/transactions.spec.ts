884a2c125885b219c2d8492e9e552aa85c6ebe4a
Paulo Barros <paulo.barros@lambda3.com.br>
2024-10-29 11:30:07 -0300

    Criando API Rest com Node.js - 04 - Testando listagem de transações

--- 02-criando-api-rest-com-nodejs/test/transactions.spec.ts
+++ 02-criando-api-rest-com-nodejs/test/transactions.spec.ts
@@ -1,7 +1,9 @@
 import {describe} from 'node:test'
 import {app} from '../src/app'
 import request from 'supertest'
-import {it, beforeAll, afterAll} from 'vitest'
+import {it, beforeAll, afterAll, expect} from 'vitest'
+import {a} from 'vitest/dist/chunks/suite.B2jumIFP'
+import {object} from 'zod'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -17,10 +19,37 @@ describe('Transactions routes', () => {
       .agent(app.server)
       .post('/transactions')
       .send({
-        title: 'Salário',
+        title: 'New Transaction',
         type: 'credit',
         amount: 8000,
       })
       .expect(201)
+  })
+
+  // jamais criar um teste que dependa de outro teste
+  it('should be able to list all transactions', async () => {
+    // criar uma transação
+    const createTransactionResponse = await request
+      .agent(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        type: 'credit',
+        amount: 5000,
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request
+      .agent(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies!)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
