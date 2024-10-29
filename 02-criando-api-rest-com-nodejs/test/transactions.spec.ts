c912d308e3a23d7db5a1e411bef94c5e3ebe0aa6
Paulo Barros <paulo.barros@lambda3.com.br>
2024-10-29 11:14:20 -0300

    Criando API Rest com Node.js - 04 - Categorizando os testes

--- 02-criando-api-rest-com-nodejs/test/transactions.spec.ts
+++ 02-criando-api-rest-com-nodejs/test/transactions.spec.ts
@@ -1,23 +1,26 @@
+import {describe} from 'node:test'
 import {app} from '../src/app'
 import request from 'supertest'
-import {test, beforeAll, afterAll} from 'vitest'
+import {it, beforeAll, afterAll} from 'vitest'
 
-beforeAll(async () => {
-  await app.ready() // o fastify precisa estar pronto para executar o teste pois todos os plugins são assíncronos
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready() // o fastify precisa estar pronto para executar o teste pois todos os plugins são assíncronos
+  })
 
-afterAll(async () => {
-  await app.close() // remove a aplicação da memória
-})
+  afterAll(async () => {
+    await app.close() // remove a aplicação da memória
+  })
 
-test('should create a new transaction', async () => {
-  await request
-    .agent(app.server)
-    .post('/transactions')
-    .send({
-      title: 'Salário',
-      type: 'credit',
-      amount: 8000,
-    })
-    .expect(201)
+  it('should be able to create a new transaction', async () => {
+    await request
+      .agent(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Salário',
+        type: 'credit',
+        amount: 8000,
+      })
+      .expect(201)
+  })
 })
