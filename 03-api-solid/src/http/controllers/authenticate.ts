8e1e7b6bf7ecd54dc2281c9f44373174bc67f98d
Paulo Barros <paulo.barros@lambda3.com.br>
2024-11-28 14:13:07 -0300

    API Node.js com SOLID - 05 - Caso de uso de perfil

--- 03-api-solid/src/http/controllers/authenticate.ts
+++ 03-api-solid/src/http/controllers/authenticate.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod'
 import {FastifyReply, FastifyRequest} from 'fastify'
-import {InvalidCredentialsError} from '@/use-cases/errors/invalide-credentials.error'
+import {InvalidCredentialsError} from '@/use-cases/errors/invalid-credentials.error'
 import {makeAuthenticateUseCase} from '@/use-cases/factories/make-authenticate-user-case'
 
 export async function authenticate(
--- 03-api-solid/src/use-cases/get-user-profile.spec.ts
+++ 03-api-solid/src/use-cases/get-user-profile.spec.ts
@@ -3,41 +3,33 @@ import {AuthenticateUseCase} from './authenticate'
 import {InMemoryUsersRepository} from '@/repositories/in-memory/users.repository'
 import {hash} from 'bcryptjs'
 import {InvalidCredentialsError} from './errors/invalid-credentials.error'
+import {GetUserProfileUseCase} from './get-user-profile'
+import {ResourceNotFoundError} from './errors/resource-not-found.error'
 
 let usersRepository: InMemoryUsersRepository
-let sut: AuthenticateUseCase
+let sut: GetUserProfileUseCase
 
-describe('AuthenticateUseCase', () => {
+describe('Get User Profile Use Case', () => {
   beforeEach(() => {
     usersRepository = new InMemoryUsersRepository()
-    sut = new AuthenticateUseCase(usersRepository)
+    sut = new GetUserProfileUseCase(usersRepository)
   })
 
-  it('should be able to authenticate', async () => {
-    await usersRepository.create({
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
     })
 
     const {user} = await sut.execute({
-      email: 'johndoe@example.com',
-      password: '123456',
+      userId: createdUser.id,
     })
 
-    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
   })
 
-  it('should not be able to authenticate with wrong email', async () => {
-    await expect(() =>
-      sut.execute({
-        email: 'johndoe@example.com',
-        password: '123456',
-      }),
-    ).rejects.toBeInstanceOf(InvalidCredentialsError)
-  })
-
-  it('should not be able to authenticate with wrong password', async () => {
+  it('should not be able get the user profile with wrong id', async () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -46,9 +38,8 @@ describe('AuthenticateUseCase', () => {
 
     await expect(() =>
       sut.execute({
-        email: 'johndoe@example.com',
-        password: '1234567',
+        userId: 'non-existing-id',
       }),
-    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
