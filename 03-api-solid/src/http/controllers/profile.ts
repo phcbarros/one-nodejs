f0b3421a225589f99eb3a6581841338c0f422b98
paulo.ubarros <paulo.ubarro@tivit.com>
2026-01-26 16:16:19 -0300

    API Node.js com SOLID - 06 - Controller de perfil

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,9 +1,14 @@
+import {makeGetUserProfileUseCase} from '@/use-cases/factories/make-get-user-profile-use-case'
 import {FastifyReply, FastifyRequest} from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
+  const {user} = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  console.log(request.user.sub)
-
-  return reply.status(200).send()
+  return reply.status(200).send({
+    ...user,
+    password_hash: undefined,
+  })
 }
--- 03-api-solid/src/http/controllers/routes.ts
+++ 03-api-solid/src/http/controllers/routes.ts
@@ -2,11 +2,12 @@ import {FastifyInstance} from 'fastify'
 import {register} from './register'
 import {authenticate} from './authenticate'
 import {profile} from './profile'
+import {verifyJWT} from '../middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   /** Authenticated routes */
-  app.get('/me', profile)
+  app.get('/me', {onRequest: [verifyJWT]}, profile)
 }
