8332011834f1eb6f141000d67065171c8c798146
paulo.ubarros <paulo.ubarro@tivit.com>
2026-01-26 14:48:44 -0300

    API Node.js com SOLID - 06 - Princípios da autenticação JWT

--- 03-api-solid/src/http/controllers/routes.ts
+++ 03-api-solid/src/http/controllers/routes.ts
@@ -1,8 +1,12 @@
 import {FastifyInstance} from 'fastify'
 import {register} from './register'
 import {authenticate} from './authenticate'
+import {profile} from './profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated routes */
+  app.get('/me', profile)
 }
