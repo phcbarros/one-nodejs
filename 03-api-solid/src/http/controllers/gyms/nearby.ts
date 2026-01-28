e7a2d9051bd7b626d6457c84d59b7326681dd5bf
paulo.ubarros <paulo.ubarro@tivit.com>
2026-01-28 17:05:38 -0300

    API Node.js com SOLID - 06 - Outros controllers da academia

---
+++ 03-api-solid/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,28 @@
+import {makeCreateGymUseCase} from '@/use-cases/factories/make-create-gym-use-case'
+import {makeFetchNearbyGymsUseCase} from '@/use-cases/factories/make-fetch-near-by-gyms-use-case'
+import {makeSearchGymsUseCase} from '@/use-cases/factories/make-search-gyms-use-case'
+import {FastifyReply, FastifyRequest} from 'fastify'
+import {z} from 'zod'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((val) => Math.abs(val) <= 90, {
+      message: 'Latitude must be between -90 and 90',
+    }),
+    longitude: z.number().refine((val) => Math.abs(val) <= 180, {
+      message: 'Longitude must be between -180 and 180',
+    }),
+  })
+
+  const {latitude, longitude} = nearbyGymQuerySchema.parse(request.query)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+  const gyms = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
