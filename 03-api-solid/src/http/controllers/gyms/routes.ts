f78e84e562d4bb8e23b83b91442726e4b7174d08
paulo.ubarros <paulo.ubarro@tivit.com>
2026-02-02 09:58:30 -0300

    API Node.js com SOLID - 07 - Autorização por cargos (RBAC)

--- 03-api-solid/src/http/controllers/gyms/routes.ts
+++ 03-api-solid/src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import {verifyJWT} from '@/http/middlewares/verify-jwt'
 import {create} from './create'
 import {search} from './search'
 import {nearby} from './nearby'
+import {verifyUserRole} from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   /** Authenticated routes */
@@ -11,5 +12,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', {onRequest: [verifyUserRole('ADMIN')]}, create)
 }
--- 03-api-solid/src/http/controllers/users/authenticate.ts
+++ 03-api-solid/src/http/controllers/users/authenticate.ts
@@ -22,7 +22,9 @@ export async function authenticate(
 
     // nunca colocar informações sensíveis no token
     const token = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
@@ -31,7 +33,9 @@ export async function authenticate(
     )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
