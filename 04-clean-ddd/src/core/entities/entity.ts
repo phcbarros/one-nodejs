b38e73c04548427f982f55d712478a23c1716ddf
paulo.ubarros <paulo.ubarro@tivit.com>
2026-02-05 15:19:43 -0300

    Clean & DDD - 01 - ID das entidades

--- 04-clean-ddd/src/core/entities/entity.ts
+++ 04-clean-ddd/src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import {randomUUID} from 'node:crypto'
+import {UniqueEntityID} from './unique-entity-id'
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityID
   protected props: Props
 
   get id() {
     return this._id
   }
 
-  protected constructor(props: Props, id?: string) {
+  constructor(props: Props, id?: string) {
     this.props = props
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityID(id)
   }
 }
