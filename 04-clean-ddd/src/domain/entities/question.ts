7b201a3b4bef6ed64f14e5740e04c0c1cb2839c0
paulo.ubarros <paulo.ubarro@tivit.com>
2026-02-05 15:24:02 -0300

    Clean & DDD - 01 - Mapeando propriedades

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,11 +1,15 @@
+import {UniqueEntityID} from '@/core/entities/unique-entity-id'
 import {Slug} from './value-objects/slug'
 import {Entity} from '@/core/entities/entity'
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps {
