7c7694f31252a32232535bece81aa4b85b4b5851
paulo.ubarros <paulo.ubarro@tivit.com>
2026-02-11 11:19:34 -0300

    Clean & DDD - 02 - Gerando dados fictícios

--- 04-clean-ddd/test/factories/make-question.ts
+++ 04-clean-ddd/test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import {faker} from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example question',
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
