a1e36717b35a72ff4637150945a867a63c1f1989
paulo.ubarros <paulo.ubarro@tivit.com>
2026-02-11 11:06:30 -0300

    Clean & DDD - 02 - Caso de uso: Buscar pergunta pelo slug

--- 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
+++ 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,6 @@
 import {Question} from '@/domain/forum/enterprise/entities/question'
 
 export interface QuestionsRepository {
+  fetchBySlug(slug: string): Promise<Question | null>
   create(question: Question): Promise<void>
 }
---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,34 @@
+import {CreateQuestionUseCase} from './create-question'
+import {InMemoryQuestionsRepository} from 'test/repositories/in-memory-questions-repository'
+import {GetQuestionBySlugUseCase} from './get-question-by-slug'
+import {Slug} from '@/domain/forum/enterprise/entities/value-objects/slug'
+import {Question} from '@/domain/forum/enterprise/entities/question'
+import {UniqueEntityID} from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  test('it should be able to fetch an question by slug', async () => {
+    // usou a entidade Question para criar a pergunta e depois inseriu direto no banco em memory
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityID('author-1'),
+      title: 'New question',
+      content: 'Question content',
+      slug: Slug.create('example-question'),
+    })
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    const {question} = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.slug.value).toBe('example-question')
+  })
+})
