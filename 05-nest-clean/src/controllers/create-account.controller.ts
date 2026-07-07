37730f9ab9c83271b94b422d45a6c07509164431
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-07 11:46:38 -0300

    Nestjs & Clean - 01 - Gerando hash da senha

--- 05-nest-clean/src/controllers/create-account.controller.ts
+++ 05-nest-clean/src/controllers/create-account.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
 } from '@nestjs/common'
 import {PrismaService} from 'src/prisma/prisma.service'
+import {hash} from 'bcryptjs'
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -27,11 +28,13 @@ export class CreateAccountController {
       throw new ConflictException('User with this email already exists')
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     })
   }
