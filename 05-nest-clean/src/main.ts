40a007b32fd5a495d6b47c6fa74044c2adcaab1a
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-07 14:19:44 -0300

    Nestjs & Clean - 01 - Usando ConfigModule no Nest.js

--- 05-nest-clean/src/main.ts
+++ 05-nest-clean/src/main.ts
@@ -1,8 +1,15 @@
 import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
+import {ConfigService} from '@nestjs/config'
+import {Env} from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3333)
+
+  // informa ao nest que as env vars foram validadas passando true no segundo argumento
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', {infer: true})
+
+  await app.listen(port)
 }
 bootstrap()
