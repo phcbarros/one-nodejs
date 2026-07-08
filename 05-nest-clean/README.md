2bda915fc28c24153c8e5529f7dcec1f53be318b
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-08 14:58:02 -0300

    Nestjs & Clean - 01 - Controller de criação de pergunta

--- 05-nest-clean/README.md
+++ 05-nest-clean/README.md
@@ -1,3 +1,9 @@
 Chave private e publica do JWT foram geradas no site
 
 https://cryptotools.net/rsagen
+
+## Prisma Studio
+
+```shell
+npx prisma studio --url="postgresql://postgres:docker@localhost:5432/nest-clean"
+```
--- 05-nest-clean/src/controllers/create-question.controller.ts
+++ 05-nest-clean/src/controllers/create-question.controller.ts
@@ -1,16 +1,52 @@
-import {Controller, Post, UseGuards} from '@nestjs/common'
+import {Body, Controller, Post, UseGuards} from '@nestjs/common'
 import {JwtAuthGuard} from 'src/auth/jwt.auth.guard'
 import {CurrentUser} from 'src/auth/current-user.decorator'
 import {UserPayload} from 'src/auth/jwt.strategy'
+import {z} from 'zod'
+import {ZodValidationPipe} from 'src/pipes/zod-validation.pipe'
+import {PrismaService} from 'src/prisma/prisma.service'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const {title, content} = body
+    const {sub: userId} = user
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug,
+        authorId: userId,
+      },
+    })
+
     return 'ok'
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
