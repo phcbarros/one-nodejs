a8669792987059eab6f4502ebcfc98ea07050810
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-09 15:22:21 -0300

    Nestjs & Clean - 02 - Criando schema do Prisma

--- 05-nest-clean/src/domain/forum/enterprise/entities/question.ts
+++ 05-nest-clean/src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import {QuestionBestAnswerChosenEvent} from './events/question-best-answer-chose
 
 export interface QuestionProps {
   authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
+  bestAnswerId?: UniqueEntityID | null
   title: string
   content: string
   slug: Slug
@@ -73,19 +73,21 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-    if (bestAnswerId === undefined) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
+    if (bestAnswerId === undefined || bestAnswerId === null) {
       return
     }
 
     if (
       this.props.bestAnswerId === undefined ||
-      !this.props.bestAnswerId.equals(bestAnswerId)
+      this.props.bestAnswerId === null ||
+      !bestAnswerId.equals(this.props.bestAnswerId)
     ) {
       this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
     }
 
     this.props.bestAnswerId = bestAnswerId
+
     this.touch()
   }
 
