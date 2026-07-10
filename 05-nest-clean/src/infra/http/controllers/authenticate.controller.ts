f76eb6f4c0448300cc588b378580dc19851a6fd3
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-10 11:14:32 -0300

    Nestjs & Clean - 02 - Tratando erros nos controllers

--- 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
+++ 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Inject,
@@ -17,6 +18,7 @@ import {HASHER_COMPARER} from '@/domain/forum/application/cryptography/hash-comp
 import {z} from 'zod'
 import {ZodValidationPipe} from '../pipes/zod-validation.pipe'
 import {AuthenticateStudentUseCase} from '@/domain/forum/application/use-cases/authenticate-student'
+import {WrongCredentialsError} from '@/domain/forum/application/use-cases/errors/wrong-credentials-error'
 
 const authenticateBodySchema = z.object({
   email: z.string(),
@@ -40,7 +42,14 @@ export class AuthenticateController {
     })
 
     if (result.isLeft()) {
-      throw new UnauthorizedException()
+      const error = result.value
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
 
     const accessToken = result.value.accessToken
