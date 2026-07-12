725bd8fcc9106c8b1679ece2c5341bc0dce4c750
paulo.ubarros <paulo.ubarro@tivit.com>
2026-07-12 17:56:49 -0300

    Nestjs & Clean - 02 - Rotas privadas por padrão

--- 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
+++ 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
@@ -19,6 +19,7 @@ import {z} from 'zod'
 import {ZodValidationPipe} from '../pipes/zod-validation.pipe'
 import {AuthenticateStudentUseCase} from '@/domain/forum/application/use-cases/authenticate-student'
 import {WrongCredentialsError} from '@/domain/forum/application/use-cases/errors/wrong-credentials-error'
+import {Public} from '@/infra/auth/public'
 
 const authenticateBodySchema = z.object({
   email: z.string(),
@@ -28,6 +29,7 @@ const authenticateBodySchema = z.object({
 type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
+@Public()
 export class AuthenticateController {
   constructor(private authenticateStudent: AuthenticateStudentUseCase) {}
 
